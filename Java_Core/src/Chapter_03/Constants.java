7e7a34e94339fe8e0ed39ce0d727e23894b6e502
luisandre69 <luisandre_69@hotmail.com>
2018-07-24 21:30:42 +0100

    LA_Chapter 3
    ---------Simple_Java_Program----------
    creation of class to return Hello World!
    -------------Constants.java--------------
    creation of  method Constants2 that returns paper size in cent
    creation of final double CM_PER_INCH to define a constant {final} variable
    call the method on the main to return the result of the method Constants2

---
+++ Java_Core/src/Chapter_03/Constants.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Chapter_03;
+
+/**
+ *
+ * @author Luis Abreu
+ */
+public class Constants {
+    //calling the final(const) variable outside the main class to be avaiable to any class
+    public static final double CM_PER_INCH = 2.54;
+    
+    public static void Constants2(){
+            double paperWidth = 8.5;
+        double paperHeight = 11;
+        System.out.println("Paper size in centimeters: " + 
+         paperWidth* CM_PER_INCH + " by " +
+         paperHeight * CM_PER_INCH);
+}
+    public static void main(String[] args){
+        
+        //calling the final variable inside will only be avaiable inside the class
+        //final double CM_PER_INCH = 2.54;
+        Constants2();
+    }
+            
+}
---
+++ Java_Core/src/Chapter_03/Simple_Java_Program.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Chapter_03;
+
+/**
+ *
+ * @author Luis Abreu
+ */
+public class Simple_Java_Program {
+    public static void main (String[] args){
+        System.out.println("Hello World!");
+    }
+}
